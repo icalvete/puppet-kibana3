8a24e0fcde6ef13d4875df2294594f9ed39fcec4
Israel Calvete <icalvete@gmail.com>
2014-05-24 18:33:13 +0200

    Adding ServerAlias as parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,25 @@
 class kibana3 (
 
-  $webserver_user  = $kibana3::params::webserver_user,
-  $webserver_group = $kibana3::params::webserver_group,
-  $repo_scheme     = $kibana3::params::repo_scheme,
-  $repo_domain     = $kibana3::params::repo_domain,
-  $repo_port       = $kibana3::params::repo_port,
-  $repo_user       = $kibana3::params::repo_user,
-  $repo_pass       = $kibana3::params::repo_pass,
-  $repo_path       = $kibana3::params::repo_path,
-  $repo_resource   = $kibana3::params::package
+  $org_domain           = $kibana3::params::org_domain,
+  $webserver_user       = $kibana3::params::webserver_user,
+  $server_alias         = undef,
+  $webserver_group      = $kibana3::params::webserver_group,
+  $repo_scheme          = $kibana3::params::repo_scheme,
+  $repo_domain          = $kibana3::params::repo_domain,
+  $repo_port            = $kibana3::params::repo_port,
+  $repo_user            = $kibana3::params::repo_user,
+  $repo_pass            = $kibana3::params::repo_pass,
+  $repo_path            = $kibana3::params::repo_path,
+  $repo_resource        = $kibana3::params::package,
+  $elasticsearch_server = $kibana3::params::elasticsearch_server
 
 ) inherits kibana3::params {
+
+  if $server_alias {
+    if ! is_array($server_alias) {
+      fail('server_alias parameter must be un array')
+    }
+  }
 
   anchor {'kibana3::begin':
     before => Class['kibana3::install']
