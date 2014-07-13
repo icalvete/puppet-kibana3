bf73f56e54ae5ca9067d1a3ab16ad7910df8cf10
Israel Calvete <icalvete@gmail.com>
2014-07-13 20:49:14 +0200

    connecting to http/https basic auth server

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,20 @@
 class kibana3 (
 
-  $org_domain           = $kibana3::params::org_domain,
-  $webserver_user       = $kibana3::params::webserver_user,
-  $server_alias         = ['kibana'],
-  $webserver_group      = $kibana3::params::webserver_group,
-  $repo_scheme          = $kibana3::params::repo_scheme,
-  $repo_domain          = $kibana3::params::repo_domain,
-  $repo_port            = $kibana3::params::repo_port,
-  $repo_user            = $kibana3::params::repo_user,
-  $repo_pass            = $kibana3::params::repo_pass,
-  $repo_path            = $kibana3::params::repo_path,
-  $repo_resource        = $kibana3::params::package,
-  $elasticsearch_server = $kibana3::params::elasticsearch_server
+  $org_domain                = $kibana3::params::org_domain,
+  $webserver_user            = $kibana3::params::webserver_user,
+  $server_alias              = ['kibana'],
+  $webserver_group           = $kibana3::params::webserver_group,
+  $repo_scheme               = $kibana3::params::repo_scheme,
+  $repo_domain               = $kibana3::params::repo_domain,
+  $repo_port                 = $kibana3::params::repo_port,
+  $repo_user                 = $kibana3::params::repo_user,
+  $repo_pass                 = $kibana3::params::repo_pass,
+  $repo_path                 = $kibana3::params::repo_path,
+  $repo_resource             = $kibana3::params::package,
+  $elasticsearch_server      = $kibana3::params::elasticsearch_server,
+  $elasticsearch_server_auth = $kibana3::params::elasticsearch_server_auth,
+  $elasticsearch_user        = $kibana3::params::elasticsearch_user,
+  $elasticsearch_user_pass   = $kibana3::params::elasticsearch_user_pass
 
 ) inherits kibana3::params {
 
