0d328badfa6776319fce099bfcc6cbf29f4868d2
Israel Calvete <icalvete@gmail.com>
2014-07-13 20:46:48 +0200

    credentials to hiera

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,8 +16,12 @@ class kibana3::params {
   $elasticsearch_server = '"+window.location.hostname+"'
   $kibana_document_root = hiera('kibana_document_root')
   $kibana_htpasswd_file = hiera('htpasswd_file')
-  $kibana_htpasswd_user = 'kibana'
-  $kibana_htpasswd_pass = 'k1b4n4'
+  $kibana_htpasswd_user = hiera('kibana_user')
+  $kibana_htpasswd_pass = hiera('kibana_pass')
 
   $config_file          = 'config.js'
+
+  $elasticsearch_server_auth = false
+  $elasticsearch_user        = hiera('kibana_user')
+  $elasticsearch_user_pass   = hiera('kibana_pass')
 }
